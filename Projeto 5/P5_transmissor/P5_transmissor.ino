650b47ae55d638cbc3d5ffe2b61109acd79b26aa
NThuesen <nicholas.thuesen@gmail.com>
2024-10-22 09:24:34 -0300

    p5

--- Projeto 5/P5_transmissor/P5_transmissor.ino
+++ Projeto 5/P5_transmissor/P5_transmissor.ino
@@ -1,53 +1,47 @@
-int txPin = 3;  // Pino digital para transmissão
-int baudRate = 9600;  // Define o baud rate
+const int txPin = 3;
 
 void setup() {
-    pinMode(txPin, OUTPUT); // Configura o pino como saída
-    Serial.begin(9600); // Para monitoramento pelo monitor serial do Arduino
+  pinMode(txPin, OUTPUT);  
+  Serial.begin(9600);  
+  Serial.println("Transmissor pronto.");
 }
 
-// Função para substituir o uso delay()
-void _sw_uart_wait_half_T() {
-    for (int i = 0; i < 1093; i++) {
-        asm("NOP");
-    }
-}
-
-void sendBit(bool bit) {
-    digitalWrite(txPin, bit ? HIGH : LOW); // Envia um bit
-    _sw_uart_wait_half_T(); // Espera pela duração de um bit
-}
-
-// Função para calcular a paridade par (even parity) usando o método do professor
 int calc_even_parity(char data) {
-    int ones = 0;
-    for (int i = 0; i < 8; i++) {
-        ones += (data >> i) & 0x01;
-    }
-    return ones % 2;
+  int ones = 0;
+  for (int i = 0; i < 8; i++) {
+    ones += (data >> i) & 0x01;
+  }
+  return ones % 2;
 }
 
-void sendChar(char c) {
-    // 1. Enviar o bit de start (LOW)
-    sendBit(LOW);
+void wait_T() {
+  for (int i = 0; i < 295; i++) {
+    asm("NOP");
+  }
+}
 
-    // 2. Enviar os 8 bits do caractere
-    for (int i = 0; i < 8; i++) {
-        bool bit = (c >> i) & 1; // Extrai o i-ésimo bit do caractere
-        sendBit(bit);
-    }
+void sendByte(char data) {
 
-    // 3. Calcular e enviar o bit de paridade
-    int parityBit = calc_even_parity(c);
-    sendBit(parityBit);
+  digitalWrite(txPin, LOW);
+  wait_T();  
 
-    // 4. Enviar o bit de stop (HIGH)
-    sendBit(HIGH);
+  for (int i = 0; i < 8; i++) {
+    int bitToSend = (data >> i) & 1;  
+    digitalWrite(txPin, bitToSend);   
+    wait_T(); 
+  }
+
+  int parity = calc_even_parity(data);
+
+  digitalWrite(txPin, parity);
+  wait_T();
+
+  digitalWrite(txPin, HIGH);
+  wait_T();
 }
 
 void loop() {
-    char teste = 'A';
-    sendChar(teste);
-    Serial.println(teste);
-    delay(1000);
+  char dataToSend = 'L'; 
+  sendByte(dataToSend);
+  delay(2);          
 }
